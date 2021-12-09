3407ae1e2e0e1f89638cb3870f96cf5c59a9eca0
undefined <undefined>
2021-12-09 20:50:45 +0530

    initial commit

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,10 @@ var shelljs = require('shelljs');
 
 /** Deploy to GitHub pages */
 gulp.task('deploy', function (done) {
-  shelljs.exec('git config --global user.email "' + process.env.USER_MAIL + '"');
-  shelljs.exec('git config --global user.name "' + process.env.USER + '"');
+  shelljs.exec('git config --global user.email "' + process.env.GITHUB_ACTOR + '@users.noreply.github.com"');
+  shelljs.exec('git config --global user.name "' + process.env.GITHUB_ACTOR + '"');
   shelljs.exec('git config pull.rebase false');
-  var gitPath = 'https://' + process.env.USER + ':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@github.com/' + process.env.USER + '/' + process.env.USER + '.github.io.git';
+  var gitPath = 'https://' + process.env.GITHUB_ACTOR + ':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@github.com/' + process.env.GITHUB_REPOSITORY + '.github.io.git';
   console.log('Clone has been started..!');
   var clone = shelljs.exec('git clone ' + gitPath + ' ' + './repo', { silent: false });
   if (clone.code !== 0) {
@@ -14,7 +14,7 @@ gulp.task('deploy', function (done) {
   } else {
     console.log('Clone has been completed..!');
     shelljs.rm('-rf', './repo/*');
-    shelljs.cp('-rf', './dist/' + process.env.USER + '/*', './repo');
+    shelljs.cp('-rf', './dist/' + process.env.GITHUB_ACTOR + '/*', './repo');
     shelljs.cd('./repo');
     shelljs.exec('git add .');
     shelljs.exec('git pull');
